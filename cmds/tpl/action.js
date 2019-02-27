f98cdf3fb496447408541533cf861d01a94f3561
sheldon <websheldoncui@gmail.com>
2019-02-28 00:02:42 +0800

    fix: 修复tpl命令无法新增模板的bug; 移除老的logger函数

--- cmds/tpl/action.js
+++ cmds/tpl/action.js
@@ -5,6 +5,7 @@ const home = require('user-home')
 const logger = require('../../lib/logger')
 const path = require('path')
 const nodeUtil = require('util')
+const Table = require('cli-table')
 
 module.exports = async function (subcmd, name, url) {
   const userTemplatesPaths = path.join(home, '.lime-cli/user-templates.json')
@@ -16,13 +17,14 @@ module.exports = async function (subcmd, name, url) {
   }
   if (subcmd === 'add') {
     if(!name || !url) {
-      utils.printError('必须填写模板名称和url，URL可以是github仓库短名称')
+      logger.fatal('必须填写模板名称和url，URL可以是github仓库短名称')
+      return
     }
     let userTplLists = await nodeUtil.promisify(readMeta)(userTemplatesPaths)
     let index = userTplLists.findIndex(item => {
       item.name === name
     })
-    if (index) {
+    if (index > -1) {
       userTplLists.splice(index, 1, {
         name: name,
         url: url,
@@ -40,10 +42,18 @@ module.exports = async function (subcmd, name, url) {
     }
     fs.writeFileSync(userTemplatesPaths, JSON.stringify(userTplLists, null, 2))
     logger.success('模板添加成功')
+    // 打印所有模板
+    let table = new Table({
+      head: ['name', 'url']
+    });
+    userTplLists.forEach(tpl => {
+      table.push([tpl.name, tpl.url])
+    })
+    console.log(table.toString());
   }
   if (subcmd === 'del') {
     if(!name) {
-      utils.printError('必须填写要删除的模板名称')
+      logger.fatal('必须填写要删除的模板名称')
     }
     let userTplLists = await nodeUtil.promisify(readMeta)(userTemplatesPaths)
     let index = userTplLists.findIndex(item => {
