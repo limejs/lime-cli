cd8e15c313767c971bae58849890a784471b4a9c
justynchen <justynchen@tencent.com>
2019-02-28 19:02:24 +0800

    fix: illegal alias

--- cmds/init/index.js
+++ cmds/init/index.js
@@ -3,7 +3,7 @@ const action = require('./action')
 
 module.exports = {
   command: 'init <dir>',
-  alias: ['create <dir>'],
+  alias: ['create'],
   usage: '[project-name]', // 用法信息
   description: '初始化lime项目',
   action,
